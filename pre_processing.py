ef23e9f3e2390432991e03ae232085cdba6b9ba8
Divya Dharani <divyadoll77@gmail.com>
2022-04-14 18:27:02 -0700

    pre processing tasks boiler plate code

--- pre_processing.py
+++ pre_processing.py
@@ -1,7 +1,33 @@
 DATASET_PATH = ''
 
-def create_cluster_model(dataset_path = DATASET_PATH):
+cluster_model = None
+cluster_model_path = ''
+
+def get_cluster_model(dataset_path = DATASET_PATH):
     pass
+    #if cluster_model is not None:
+      #   return cluster_model
+
+    #if <cluster model path> has cluster model:
+        #cluster_model =
+        #read that model and return
+
+
+    #otherwise
+    #read the dataset, clean the data, create cluster model, and dump in the cluster model path
+
+
+    # docs = [nlp(text) for text in text_list]
+    # for doc in docs:
+    #     print(doc.vector)
+
+
+    #cluster_model =
+    #store the cluster model
+    #return cluster_model
+
+def create_cluster_model(dataset_path = DATASET_PATH):
+    get_cluster_model(dataset_path)
 
 if '__name__' == '__main__':
     create_cluster_model(DATASET_PATH)
