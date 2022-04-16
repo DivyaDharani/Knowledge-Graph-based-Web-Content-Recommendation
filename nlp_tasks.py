f756792e94c3f40b8ea873fee57eae9e992cbf92
Divya Dharani <divyadoll77@gmail.com>
2022-04-16 00:33:36 -0700

    coreference resolution added

--- nlp_tasks.py
+++ nlp_tasks.py
@@ -1,8 +1,10 @@
 import spacy
 from spacy.matcher import Matcher
 from spacy import displacy
+import neuralcoref
 
 nlp = spacy.load("en_core_web_lg")
+neuralcoref.add_to_pipe(nlp)
 
 def get_subjects_and_objects(doc):
     subjects = []
@@ -116,6 +118,25 @@ def get_sentences(text):
 
 def get_noun_chunks(doc):
     return list(doc.noun_chunks)
+
+def coref_resolution(text):
+    """Function that executes coreference resolution on a given text"""
+    doc = nlp(text)
+    # fetches tokens with whitespaces from spacy document
+    tok_list = list(token.text_with_ws for token in doc)
+    for cluster in doc._.coref_clusters:
+        # get tokens from representative cluster name
+        cluster_main_words = set(cluster.main.text.split(' '))
+        for coref in cluster:
+            if coref != cluster.main:  # if coreference element is not the representative element of that cluster
+                if coref.text != cluster.main.text and bool(set(coref.text.split(' ')).intersection(cluster_main_words)) == False:
+                    # if coreference element text and representative element text are not equal and none of the coreference element words are in representative element. This was done to handle nested coreference scenarios
+                    tok_list[coref.start] = cluster.main.text + \
+                        doc[coref.end-1].whitespace_
+                    for i in range(coref.start+1, coref.end):
+                        tok_list[i] = ""
+
+    return "".join(tok_list)
 
 if __name__ == '__main__':
 
