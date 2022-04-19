9b2483f91415d0a82d9a7dbbf5be93ff4a64b9a9
Divya Dharani <divyadoll77@gmail.com>
2022-04-18 19:40:05 -0700

    knowledge graph constructing if None

--- web_recommendation.py
+++ web_recommendation.py
@@ -1,15 +1,19 @@
 import spacy
 from nlp_tasks import get_entities
 from collections import Counter
+from knowledge_graph import construct_knowledge_graph
 
 #project module imports:
 from clustering import get_cluster_memberships
 
 nlp = spacy.load("en_core_web_sm")
 
-def recommend_web_articles(user_knowledge_graph_df):
+def recommend_web_articles(texts, user_knowledge_graph_df = None):
     recommendation_result = None
     try:
+        if user_knowledge_graph_df is None:
+            user_knowledge_graph_df = construct_knowledge_graph(texts)
+
         entities = list(user_knowledge_graph_df['Entity List'])
         entities_joined = []
         for ent_list in entities:
@@ -20,7 +24,7 @@ def recommend_web_articles(user_knowledge_graph_df):
         vectors = [doc.vector for doc in docs]
         cluster_memberships = get_cluster_memberships(vectors)
         membership_counts = Counter(cluster_memberships)
-
+        
         #To do further:
         #sort the membership counts based on the count in descending order
         #then add recommendation logic (using get_random item from cluster) - the logic should decide how many items to pick from which clusters
